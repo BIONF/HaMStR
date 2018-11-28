7e5261d447b7899f882372a85b35f9b40cd73d82
Julian Dosch <julian@leslie.izn-ffm.intern>
2018-11-28 14:02:02 +0100

    parseOneseq update by Vinh

--- bin/visuals/parseOneSeq.pl
+++ bin/visuals/parseOneSeq.pl
@@ -9,13 +9,11 @@ use Getopt::Long;
 use File::Basename;
 use Env qw(ONESEQDIR);
 
-
 #######################
 # NAME:         parseOneSep.pl
 # AUTHOR:       Vinh Tran, tran@bio.uni-frankfurt.de
-# MODIFIED:     Holger Bergmann, bergmann@bio.uni-frankfurt.de
 # DESCRIPTION:  parsing output of oneSeq.pl to create input to phyloprofile app
-# DATE:         16.12.2016
+# DATE:         05.02.2018
 # SUPPORT:      sge,qsub
 # STATUS:       devo
 
@@ -35,35 +33,30 @@ if (!(defined $path) or !(-e $path)) {
 $path =~ s/\/$//;
 
 
-
 sub usage {
     my $msg = shift;
-    print "example: perl parseOneSeq.pl -i oneseqOutput -o output.matrix\n";
-    print "-i\thamstr oneseq output file (*.extended.profile)\n";
+    print "example: perl parseOneSeq.pl -i oneseqOutput -o output.phyloprofile\n";
+    print "-i\thamstr oneseq output folder (contains *.extended.profile)\n";
     print "-o\tOutput file\n";
     die $msg."\n";
 }
 
 # global variables
-our($opt_i,$opt_t,$opt_o);
-getopts('i:t:o:');
+our($opt_i,$opt_o);
+getopts('i:o:');
 
 # sanity checks;
-my $oneseqFile = ($opt_i) ? $opt_i : usage("ERROR: No oneseq output file given\n");
+my $oneseqFolder = ($opt_i) ? $opt_i : usage("ERROR: No oneseq output folder given\n");
 my $out = ($opt_o) ? $opt_o : usage("ERROR: No output file given\n");
 
 ### MAIN
-my @allOutFiles = glob("$oneseqFile");
+my @allOutFiles = glob("$oneseqFolder/*.extended.profile");
 unless(@allOutFiles){
 	usage("ERROR: No extended.profile file found!\n");
 }
 
-my %taxaList;	# list of all taxa
-my %allGenes;	# list of all genes
-my %fas0;	# $fas0{$protID#$taxonID} = FAS_score
-my %fas1;	# $fas1{$protID#$taxonID} = FAS_score
-my $countercheck;
-
+open(OUT,">$out") || die "Cannot open $out!\n";
+print OUT "geneID\tncbiID\torthoID\tFAS_F\tFAS_B\n";
 foreach my $file(@allOutFiles){
 	open(IN,$file) || die "Cannot open $file!\n";
 	my @in = <IN>;
@@ -71,104 +64,23 @@ foreach my $file(@allOutFiles){
 
 	### get taxon ID, ortho ID and FAS scores from each oneseq output file
 	foreach my $line(@in){
-		chomp($line);	# Arath01153|aquco_5436@218851@1|28258|1	0.99512260472
+		chomp($line);	# STRADB|ANOGA@7165@1|Q7Q3C2|1    0.999591652     0.66387534112
 #		print $line,"\n";
-		$line =~ s/\|/\t/g;		# Arath01153	aquco_5436@218851@1	28258	1	0.99512260472
+		$line =~ s/\|/\t/g;
 		my @tmp = split(/\t/,$line);
 
-		my $geneID = $tmp[0];
-		my $fas0 = "NA";
-		my $fas1 = "NA";
-		if($tmp[4]){
-                    $fas1 = $tmp[4];
-                }
-		if($tmp[5]){
-                    $fas0 = $tmp[5];
-                    $countercheck = 1;
-                }
-		
-		my @hit = split(/\@/,$tmp[1]);
-		my $taxonID = $hit[1];
-#		my $hitID = $hit[0].":".$tmp[2];
-		my $hitID = $tmp[0]."|".$tmp[1]."|".$tmp[2]."|".$tmp[3];
-                my $rep = $tmp[3]; #true if rep. ortholog
-#		print "$geneID - $taxonID - $fas";<>;
+		my @ncbiID = split(/@/,$tmp[1]);
+		my $orthoID = $tmp[0]."|".$tmp[1]."|".$tmp[2]."|".$tmp[3];
 
-		### save to %taxaList, %allGenes and %fas
-		$taxaList{"ncbi$taxonID"} = 1;
-		unless($fas0{"$geneID#ncbi$taxonID"}){
-			$fas0{"$geneID#ncbi$taxonID"} = $hitID."#".$fas0;
-			$fas1{"$geneID#ncbi$taxonID"} = $hitID."#".$fas1;
-		} else {
-			if($fas0{"$geneID#ncbi$taxonID"} =~ /\#NA/){
-				$fas0{"$geneID#ncbi$taxonID"} = $hitID."#".$fas0;
-			} else {
-				unless($fas0 eq "NA"){
-					my @fasTMP = split(/\#/,$fas0{"$geneID#ncbi$taxonID"});		
-					if($fasTMP[1] < $fas0){
-						$fas0{"$geneID#ncbi$taxonID"} = $hitID."#".$fas0;
-					}
-                                        if($fasTMP[1] == $fas0){
-                                                if($rep){
-                                                        $fas0{"$geneID#ncbi$taxonID"} = $hitID."#".$fas0;
-                                                }
-					}
-				}
-			}
-			
-			if($fas1{"$geneID#ncbi$taxonID"} =~ /\#NA/){
-				$fas1{"$geneID#ncbi$taxonID"} = $hitID."#".$fas1;
-			} else {
-				unless($fas1 eq "NA"){
-					my @fasTMP = split(/\#/,$fas1{"$geneID#ncbi$taxonID"});		
-					if($fasTMP[1] < $fas1){
-						$fas1{"$geneID#ncbi$taxonID"} = $hitID."#".$fas1;
-					}
-                                        if($fasTMP[1] == $fas1){
-                                                if($rep){
-                                                        $fas1{"$geneID#ncbi$taxonID"} = $hitID."#".$fas1;
-                                                }
-					}
-				}
-			}
-		}
-		$allGenes{$geneID} = 1;
+		my $fas_F = "0.0";
+		if($tmp[4] > 0){$fas_F = $tmp[4];}
+		my $fas_B = "0.0";
+    if(defined $tmp[5] && $tmp[5] ne "0"){$fas_B = $tmp[5];}
+
+		print OUT "$tmp[0]\tncbi$ncbiID[1]\t$orthoID\t$fas_F\t$fas_B\n";
 	}
 }
 
-### print output
-if ($countercheck){open(OUT0,">".$out."_0.matrix") || die "Cannot create $out\_0.matrix!\n";}
-open(OUT1,">".$out."_1.matrix") || die "Cannot create $out\_1.matrix!\n";
-
-my @allTaxa = sort keys %taxaList;
-my $allTaxa = join("\t",@allTaxa);
-if ($countercheck){print OUT0 "geneID\t$allTaxa\n"};
-print OUT1 "geneID\t$allTaxa\n";
-
-foreach my $gene(sort keys %allGenes){
-	if ($countercheck){print OUT0 $gene;}
-	print OUT1 $gene;
-	
-	foreach my $taxon(sort @allTaxa){
-		if($fas0{"$gene#$taxon"}){
-			if ($countercheck){print OUT0 "\t",$fas0{"$gene#$taxon"};}
-		} else {
-			if ($countercheck){print OUT0 "\t","NA";}
-		}
-		
-		if($fas1{"$gene#$taxon"}){
-			print OUT1 "\t",$fas1{"$gene#$taxon"};
-		} else {
-			print OUT1 "\t","NA";
-		}
-	}
-	if ($countercheck){print OUT0 "\n";}
-	print OUT1 "\n";
-}
-if ($countercheck){close (OUT0);}
-close (OUT1);
-
-print "Finished! Check output file\n\t",$out,"_1.matrix\n";
-if ($countercheck){print "\t".$out,"_0.matrix\n";}
-
+close (OUT);
+print "Finished! Check output file $out\n";
 exit;
