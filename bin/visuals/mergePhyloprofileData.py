7b952c16d0d5c04f77d825235274e210c366ab3d
Julian Dosch <julian@leslie.izn-ffm.intern>
2019-02-04 10:41:11 +0100

    added merge script for phyloprofile output

---
+++ bin/visuals/mergePhyloprofileData.py
@@ -0,0 +1,40 @@
+from sys import exit
+from sys import argv
+from os import listdir as ldir
+from os.path import isfile
+
+
+def main(directory, out):
+    if not directory[-1] == '/':
+        directory += '/'
+    phyloprofile = open(out + '.phyloprofile', 'w')
+    domains_0 = open(out + '_0.domains', 'w')
+    domains_1 = open(out + '_1.domains', 'w')
+    phyloprofile.write('geneID\tncbiID\torthoID\tFAS_F\tFAS_B\n')
+    for infile in ldir(directory):
+        if infile.endswith('.phyloprofile') and not infile == out + '.phyloprofile':
+            with open(directory + infile, 'r') as reader:
+                reader.readline()
+                line = reader.readline()
+                while line:
+                    phyloprofile.write(line)
+                    line = reader.readline()
+        elif infile.endswith('_0.domains') and not infile == out + '_0.domains':
+            with open(directory + infile, 'r') as reader:
+                line = reader.readline()
+                while line:
+                    domains_0.write(line)
+                    line = reader.readline()
+        elif infile.endswith('_1.domains') and not infile == out + '_1.domains':
+            with open(directory + infile, 'r') as reader:
+                line = reader.readline()
+                while line:
+                    domains_0.write(line)
+                    line = reader.readline()
+    phyloprofile.close()
+    domains_0.close()
+    domains_1.close()
+
+
+if __name__ == "__main__":
+    exit(main(argv[1], argv[2]))
