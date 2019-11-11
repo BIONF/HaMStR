b6c0db84a50603788e9e134bbf22a4cdd7abc0c0
trvinh <trvinh@gmail.com>
2019-11-11 22:38:24 +0100

    install blast hmmer clustal and mafft from conda

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,27 +3,73 @@
 # check dependencies
 echo "-------------------------------------"
 echo "Checking dependencies..."
+if [ -z "$(which R)" ]; then conda install -y r; fi
+# conda install -y -c bioconda perl-bioperl
+# conda install -y -c bioconda perl-bioperl-core
 
 dependencies=(
-  blastp
+  blastp # blast
   genewise
-  hmmsearch
-  hmmbuild
+  hmmsearch # hmmer (for both hmmsearch and hmmbuild)
   clustalw
-  linsi
-  perl
-  python
+  mafft # for linsi
 )
 
 flag=0
-# for i in "${dependencies[@]}"; do
-#   echo "$i"
-#   msg="$(which $i)"
-#   if [ -z "$msg" ]; then
-#     echo "$i not found. Please install it to HaMStR!"
-#     flag=1
-#   fi
-# done
+for i in "${dependencies[@]}"; do
+  if [ -z "$(which $i)" ]; then
+    echo $i
+    tool=$i
+    if [ "$tool" = "blastp" ]; then
+      conda install -y -c bioconda blast
+    elif [ "$tool" = "hmmsearch" ]; then
+      conda install -y -c bioconda hmmer
+    elif [ "$tool" = "genewise" ]; then
+      # echo "install genewise"
+      # wget http://www.ebi.ac.uk/~birney/wise2/wise2.4.1.tar.gz
+      # tar xfv wise2.4.1.tar.gz
+      # cd wise2.4.1/src
+      # # Here we need to modify makefile to replace "CC = cc" with "CC = gcc" in Line 25 and save the change
+      # find ./ -name makefile | xargs sed -i 's/glib-config/pkg-config --libs glib-2.0/'
+      # cd ./HMMer2/
+      # sed 's/getline/getline_new/' sqio.c  > a &&  mv a sqio.c
+      # cd ../models/
+      # # Change phasemodel.c to replace "if( !isnumber(line[0]) ) {" with "if( !isdigit(line[0]) ) {" in Line 23
+      # cd ..
+      # make all
+    else
+      conda install -y -c bioconda $i
+    fi
+  fi
+  #
+  # if [ -z "$(which $i)" ]; then
+  #   echo "$tool"
+  #   if [ tool=="genewise" ]; then
+  #     echo "JA"
+  #     echo "genewise"
+  #     # wget http://www.ebi.ac.uk/~birney/wise2/wise2.4.1.tar.gz
+  #     # tar xfv wise2.4.1.tar.gz
+  #     # cd wise2.4.1/src
+  #     # # Here we need to modify makefile to replace "CC = cc" with "CC = gcc" in Line 25 and save the change
+  #     # find ./ -name makefile | xargs sed -i 's/glib-config/pkg-config --libs glib-2.0/'
+  #     # cd ./HMMer2/
+  #     # sed 's/getline/getline_new/' sqio.c  > a &&  mv a sqio.c
+  #     # cd ../models/
+  #     # # Change phasemodel.c to replace "if( !isnumber(line[0]) ) {" with "if( !isdigit(line[0]) ) {" in Line 23
+  #     # cd ..
+  #     # make all
+  #   else
+  #     conda install -y -c bioconda $tool
+  #   fi
+  # fi
+done
+
+for i in "${dependencies[@]}"; do
+  if [ -z "$(which $i)" ]; then
+  echo "$i not found. Please install it to HaMStR!"
+  flag=1
+  fi
+done
 
 if [ "$flag" == 1 ]; then exit 1; fi
 echo "done!"
@@ -104,13 +150,13 @@ cd $CURRENT
 cd "bin/fas/Pfam/Pfam-hmms"
 FILE=Pfam-A.hmm
 if ! [ -f "$FILE" ]; then
-    echo "pfam-A.hmm"
-    # wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release//Pfam-A.hmm.gz
-    # wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz
-    # gunzip Pfam-A.hmm.gz
-    # gunzip Pfam-A.hmm.dat.gz
-    # hmmpress Pfam-A.hmm
-    # mv bin/pfam_scan.pl bin/fas/Pfam/
+  echo "pfam-A.hmm"
+  # wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release//Pfam-A.hmm.gz
+  # wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz
+  # gunzip Pfam-A.hmm.gz
+  # gunzip Pfam-A.hmm.dat.gz
+  # hmmpress Pfam-A.hmm
+  # mv bin/pfam_scan.pl bin/fas/Pfam/
 fi
 cd $CURRENT
 
@@ -148,7 +194,7 @@ perlModules=(
 for i in "${perlModules[@]}"; do
   msg=$(perldoc -l $i)
   if [[ $msg =~ *no\sdocumentation* ]]; then
-    echo "$i could not be installed!"
+  echo "$i could not be installed!"
   fi
 done
 
@@ -162,67 +208,67 @@ exit 1
 # echo $CURRENT
 #
 # if [[ $CURRENT == */HaMStR ]] || [[ $CURRENT == */hamstr ]]; then
-#     echo "Processing $CURRENT ..."
-#     echo "Downloading data from https://applbio.biologie.uni-frankfurt.de/download/hamstr_qfo/data_HaMStR.tar"
-#     wget --no-check-certificate https://applbio.biologie.uni-frankfurt.de/download/hamstr_qfo/data_HaMStR.tar
-#     if [ ! -f $CURRENT/data_HaMStR.tar ]; then
-#         echo "File not found!"
+#   echo "Processing $CURRENT ..."
+#   echo "Downloading data from https://applbio.biologie.uni-frankfurt.de/download/hamstr_qfo/data_HaMStR.tar"
+#   wget --no-check-certificate https://applbio.biologie.uni-frankfurt.de/download/hamstr_qfo/data_HaMStR.tar
+#   if [ ! -f $CURRENT/data_HaMStR.tar ]; then
+#     echo "File not found!"
+#   else
+#     CHECKSUM=$(cksum data_HaMStR.tar)
+#     echo "Checksum: $CHECKSUM"
+#     if [ "$CHECKSUM" == "557087663 1952579568 data_HaMStR.tar" ]; then
+#       echo "Extracting archive data_HaMStR.tar"
+#       tar xfv $CURRENT/data_HaMStR.tar
+#       echo "Archive data_HaMStR.tar extracted into $CURRENT"
+#       if [ ! -d $CURRENT/data_HaMStR ]; then
+#         echo "Directory $CURRENT/data_HaMStR not found!"
+#       else
+#         printf "\nMoving gene sets ...\n--------------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/genome_dir/* $CURRENT/genome_dir
+#         printf "\nMoving blast databases ...\n--------------------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/blast_dir/* $CURRENT/blast_dir
+#         printf "\nMoving annotations ...\n----------------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/weight_dir/* $CURRENT/weight_dir
+#         printf "\nMoving Taxonomy ...\n-------------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/taxonomy/* $CURRENT/taxonomy
+#         printf "\nMoving Pfam ...\n---------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/Pfam/* $CURRENT/bin/fas/Pfam
+#         printf "\nMoving SMART ...\n----------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/SMART/* $CURRENT/bin/fas/SMART
+#         printf "\nMoving CAST ...\n---------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/CAST/* $CURRENT/bin/fas/CAST
+#         printf "\nMoving COILS ...\n----------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/COILS2/* $CURRENT/bin/fas/COILS2
+#         printf "\nMoving SEG ...\n--------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/SEG/* $CURRENT/bin/fas/SEG
+#         printf "\nMoving SignalP ...\n------------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/SignalP/* $CURRENT/bin/fas/SignalP
+#         printf "\nMoving TMHMM ...\n----------------\n"
+#         #sleep 3
+#         rsync -rva data_HaMStR/TMHMM/* $CURRENT/bin/fas/TMHMM
+#         rsync -rva data_HaMStR/README* $CURRENT/
+#         printf "\nRemoving duplicated data. Please wait.\n------------------------------------\n"
+#         rm -rf $CURRENT/data_HaMStR
+#         printf "\nFinished. Data should be in place to run HaMStR.\n"
+#       fi
 #     else
-#         CHECKSUM=$(cksum data_HaMStR.tar)
-#         echo "Checksum: $CHECKSUM"
-#         if [ "$CHECKSUM" == "557087663 1952579568 data_HaMStR.tar" ]; then
-#             echo "Extracting archive data_HaMStR.tar"
-#             tar xfv $CURRENT/data_HaMStR.tar
-#             echo "Archive data_HaMStR.tar extracted into $CURRENT"
-#             if [ ! -d $CURRENT/data_HaMStR ]; then
-#                 echo "Directory $CURRENT/data_HaMStR not found!"
-#             else
-#                 printf "\nMoving gene sets ...\n--------------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/genome_dir/* $CURRENT/genome_dir
-#                 printf "\nMoving blast databases ...\n--------------------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/blast_dir/* $CURRENT/blast_dir
-#                 printf "\nMoving annotations ...\n----------------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/weight_dir/* $CURRENT/weight_dir
-#                 printf "\nMoving Taxonomy ...\n-------------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/taxonomy/* $CURRENT/taxonomy
-#                 printf "\nMoving Pfam ...\n---------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/Pfam/* $CURRENT/bin/fas/Pfam
-#                 printf "\nMoving SMART ...\n----------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/SMART/* $CURRENT/bin/fas/SMART
-#                 printf "\nMoving CAST ...\n---------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/CAST/* $CURRENT/bin/fas/CAST
-#                 printf "\nMoving COILS ...\n----------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/COILS2/* $CURRENT/bin/fas/COILS2
-#                 printf "\nMoving SEG ...\n--------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/SEG/* $CURRENT/bin/fas/SEG
-#                 printf "\nMoving SignalP ...\n------------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/SignalP/* $CURRENT/bin/fas/SignalP
-#                 printf "\nMoving TMHMM ...\n----------------\n"
-#                 #sleep 3
-#                 rsync -rva data_HaMStR/TMHMM/* $CURRENT/bin/fas/TMHMM
-#                 rsync -rva data_HaMStR/README* $CURRENT/
-#                 printf "\nRemoving duplicated data. Please wait.\n------------------------------------\n"
-#                 rm -rf $CURRENT/data_HaMStR
-#                 printf "\nFinished. Data should be in place to run HaMStR.\n"
-#             fi
-#         else
-#             echo "Something went wrong with the download. Checksum does not match."
-#         fi
+#       echo "Something went wrong with the download. Checksum does not match."
 #     fi
+#   fi
 #
 # else
-#     echo "Please change into your HaMStR directory and run install_data.sh again."
-#     echo "Exiting."
-#     exit
+#   echo "Please change into your HaMStR directory and run install_data.sh again."
+#   echo "Exiting."
+#   exit
 # fi
 # echo "done!"
