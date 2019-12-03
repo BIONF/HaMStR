7af384c27d0aaf6c0078720be4facdc0eba9aaf4
Ingo Ebersberger <ebersberger@bio.uni-frankfurt.de>
2019-12-03 11:31:57 +0100

    changed checksum to new download file

--- bin/install_data.sh
+++ bin/install_data.sh
@@ -15,7 +15,7 @@ if [[ $CURRENT == */HaMStR ]] || [[ $CURRENT == */hamstr ]]; then
     else
         CHECKSUM=$(cksum data_HaMStR.tar)
         echo "Checksum: $CHECKSUM"
-        if [ "$CHECKSUM" == "557087663 1952579568 data_HaMStR.tar" ]; then
+        if [ "$CHECKSUM" == "4100986910 5840435200 data_HaMStR.tar" ]; then
             echo "Extracting archive data_HaMStR.tar"
             tar xfv $CURRENT/data_HaMStR.tar
             echo "Archive data_HaMStR.tar extracted into $CURRENT"
