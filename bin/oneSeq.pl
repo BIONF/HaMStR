b8a036f1bcd9a6301119ce34b7aa0fc37eadcf6a
Vinh Tran <trvinh@gmail.com>
2020-02-13 11:14:12 +0100

    deleted print annoFAS command

--- bin/oneSeq.pl
+++ bin/oneSeq.pl
@@ -1244,7 +1244,6 @@ if (!$coreOnly) {
 	    # chdir($fasPath);
 	    # my $annotationCommand = "perl $fasPath/$annotation_prog -fasta=" . $seedseqFile . " -path=" . $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir" . " -name=" . $seqName . "_seed";
 		my $annotationCommand = "$annotation_prog --fasta=" . $seedseqFile . " --path=" . $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir" . " --name=" . $seqName . "_seed";
-		print $annotationCommand,"\n";
 	    system($annotationCommand);
 	}
 	## starting annotation_prog for candidate ortholog
@@ -1273,7 +1272,6 @@ if (!$coreOnly) {
 			mkdir($coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir/" . $cand_geneset . "_" . $gene_id);
 		}
 		my $annotationCommand = "$annotation_prog --fasta=" . $candseqFile . " --path=" . $weightPath . $cand_geneset . " --name=" . $gene_id . " --extract=" . $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir/" . $cand_geneset . "_" . $gene_id;
-		print $annotationCommand,"\n";
 		system($annotationCommand);
 
 		$location = $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir/" . $cand_geneset . "_" . $gene_id;
@@ -1285,7 +1283,6 @@ if (!$coreOnly) {
 		# chdir($fasPath);
 		# my $annotationCommand = "perl $fasPath/$annotation_prog -fasta=" . $candseqFile . " -path=" . $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir/" . " -name=" . $cand_geneset . "_" . $gene_id;
 		my $annotationCommand = "$annotation_prog --fasta=" . $candseqFile . " --path=" . $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir/" . " --name=" . $cand_geneset . "_" . $gene_id;
-		print $annotationCommand,"\n";
 		system($annotationCommand);
 
 		$location = $coreOrthologsPath . $seqName . "/fas_dir" . "/annotation_dir/" . $cand_geneset . "_" . $gene_id;
