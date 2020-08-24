349fbf7a8550a14f6aca25c745cf102e8ce55efe
trvinh <trvinh@gmail.com>
2020-08-24 10:56:28 +0200

    version bump

--- h1s/h1s.py
+++ h1s/h1s.py
@@ -152,7 +152,7 @@ def h1s(basicArgs, ioArgs, pathArgs, coreArgs, hamstrArgs, fasArgs, otherArgs):
         sys.exit('Problem running\n%s' % (cmd))
 
 def main():
-    version = '2.1.8'
+    version = '2.1.9'
     parser = argparse.ArgumentParser(description='You are running h1s version ' + str(version) + '.')
     parser.add_argument('--version', action='version', version=str(version))
     required = parser.add_argument_group('Required arguments')
